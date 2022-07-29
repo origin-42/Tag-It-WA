c936922b58579363d1d10221fca4c82f71737992
Origin 42 <daveplum1991@gmail.com>
2022-07-29 20:51:02 +0800

    updates for Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks-app', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tagit-wa', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
