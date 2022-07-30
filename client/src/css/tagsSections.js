b885b421b9ce4d710d5fa0635deae302a0ec6259
Origin 42 <daveplum1991@gmail.com>
2022-07-31 00:13:10 +0800

    Updating styling for manage alerts section.

--- client/src/css/tagsSections.js
+++ client/src/css/tagsSections.js
@@ -3,6 +3,7 @@ export const TagSectionsCSS = {
         moreInfoSection: {
             backgroundColor: "#045de9",
             backgroundImage: "linear-gradient(180deg, white, #09c6f9 74%)",
+            height: "200vh",
             borderTop: "10px groove blue"
         },
         moreInfoWrapper: {
@@ -16,12 +17,12 @@ export const TagSectionsCSS = {
             display: "flex",
             flexDirection: "column",
             gap: "20px",
-            borderRight: "10px solid blue",
         },
         secRight: {
             padding: "20px",
             display: "flex",
             flexDirection: "column",
+            borderLeft: "10px solid blue",
             gap: "20px",
         },
         titles: {
@@ -72,6 +73,7 @@ export const TagSectionsCSS = {
         myCommentsSection: {
             backgroundColor: "#045de9",
             backgroundImage: "linear-gradient(180deg, white, #09c6f9 74%)",
+            height: "200vh",
             borderTop: "10px groove blue"
         },
         myCommentsWrapper: {
@@ -85,12 +87,12 @@ export const TagSectionsCSS = {
             display: "flex",
             flexDirection: "column",
             gap: "20px",
-            borderRight: "10px solid blue",
         },
         secRight: {
             padding: "20px",
             display: "flex",
             flexDirection: "column",
+            borderLeft: "10px solid blue",
             gap: "20px",
         },
         titles: {
@@ -141,6 +143,7 @@ export const TagSectionsCSS = {
         alertsSection: {
             backgroundColor: "#045de9",
             backgroundImage: "linear-gradient(180deg, white, #09c6f9 74%)",
+            height: "200vh",
             borderTop: "10px groove blue"
         },
         alertsWrapper: {
@@ -154,12 +157,12 @@ export const TagSectionsCSS = {
             display: "flex",
             flexDirection: "column",
             gap: "20px",
-            borderRight: "10px solid blue",
         },
         secRight: {
             padding: "20px",
             display: "flex",
             flexDirection: "column",
+            borderLeft: "10px solid blue",
             gap: "20px",
         },
         titles: {
@@ -204,6 +207,98 @@ export const TagSectionsCSS = {
         invalid: {
             fontWeight: "bold",
             color: "red"
+        }
+    },
+    management: {
+        managementSection: {
+            backgroundColor: "#045de9",
+            backgroundImage: "linear-gradient(180deg, white, #09c6f9 74%)",
+            height: "200vh",
+            borderTop: "10px groove blue"
+        },
+        managementWrapper: {
+            display: "grid",
+            gridTemplateColumns: "1fr 2fr",
+            justifyContent: "space-around",
+            fontSize: "1.5rem"
+        },
+        secLeft: {
+            padding: "20px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+        },
+        secRight: {
+            padding: "20px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            borderLeft: "10px solid blue",
+        },
+        titles: {
+            textAlign: "center",
+            color: "blue"
+        },
+        confirmations: {
+            display: "flex",
+            justifyContent: "space-between"
+        },
+        confirmationsManagement: {
+            display: "flex",
+            justifyContent: "space-around"
+        },
+        bold: {
+            fontWeight: "bold"
+        },
+        dates: {
+            fontStyle: "italic",
+            opacity: "0.7"
+        },
+        comments: {
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px"
+        },
+        gapper: {
+            display: "flex",
+            flexDirection: "column", 
+            gap: "10px",
+            borderBottom: "5px solid blue",
+            paddingBottom: "20px",
+        },
+        gapperDesc: {
+            display: "flex",
+            flexDirection: "column", 
+            padding: "25px",
+            fontStyle: "italic",
+        },
+        previous: {
+            fontSize: "3rem",
+            color: "blue"
+        },
+        input: {
+            padding: "10px",
+            border: "2px solid black",
+            boxShadow: "2px 2px 2px black inset",
+            fontSize: "1.3rem"
+        },
+        valid: {
+            fontWeight: "bold",
+            color: "blue"
+        },
+        invalid: {
+            fontWeight: "bold",
+            color: "red"
+        },
+        yellowStyle: {
+            color: "orange"
+        },
+        options: {
+            padding: "5px",
+            borderRadius: "none",
+            border: "2px solid blue",
+            boxShadow: "1px 1px 2px 1px blue",
+            fontWeight: "bold"
         }
     }
 }
--- client/src/pages/ManageAlerts.js
+++ client/src/pages/ManageAlerts.js
@@ -5,7 +5,11 @@ import { criteriaData } from '../utils/criteriaData';
 import { GetDate } from '../utils/helper';
 import { useState } from 'react';
 
+import { TagSectionsCSS } from '../css/tagsSections';
+import { Button } from '../css/button';
+
 import { BsPersonCircle } from 'react-icons/bs';
+import { AiOutlineWarning, AiFillMessage } from 'react-icons/ai';
 
 export const ManageAlerts = ({ setCriteriaState }) => {
     if (!Auth.loggedIn()) {
@@ -51,57 +55,61 @@ export const ManageAlerts = ({ setCriteriaState }) => {
     }
 
     return (
-        <section id='manageAlertsPage'>
-            <section id='manageAlertsPageContainer'>
-                <article id='manageAlertsBox'>
-                    <div id='selectAlertCriteria'>
+        <section id='manageAlertsPage' style={TagSectionsCSS.management.managementSection}>
+            <section id='manageAlertsPageContainer' style={TagSectionsCSS.management.managementWrapper}>
+                <article id='manageAlertsBox' style={TagSectionsCSS.management.secLeft}>
+                    <div style={TagSectionsCSS.management.titles}>
+                        <h2>MANAGE ALERTS</h2>
+                    </div>
+                    <div id='selectAlertCriteria' style={TagSectionsCSS.management.confirmations}>
                         <label htmlFor='alertsCriteria'>Select Criteria</label>
-                        <select id='alertsCriteria' onChange={(e) => handleChange(e)}>
+                        <select style={TagSectionsCSS.management.options} id='alertsCriteria' onChange={(e) => handleChange(e)}>
                             <option>No selections</option>
                             {criteriaData.map((criteria) => {
-                                const subString = criteria[0].toUpperCase() + criteria.substring(1);
+                                let subString = criteria[0].toUpperCase() + criteria.substring(1);
                                 return (
                                     <option key={criteria + "alert"} value={criteria + "alert"}>{subString}</option>
                                 );
                             })};
                         </select>
                     </div>
-                    <div>
-                        <p>Criteria: </p>
-                        <p>{alertSettings.criteria[0] && alertSettings.criteria[0].toUpperCase() + alertSettings.criteria.substring(1)}</p>
+                    <div style={TagSectionsCSS.management.confirmations}>
+                        <p style={TagSectionsCSS.management.yellowStyle}><AiOutlineWarning /> Criteria: </p>
+                        <p style={TagSectionsCSS.management.yellowStyle}>{alertSettings.criteria[0] && alertSettings.criteria[0].toUpperCase() + alertSettings.criteria.replace("alert", "").substring(1)}</p>
                     </div>
-                    <div>
+                    <div style={TagSectionsCSS.management.confirmations}>
                         <p>Number of issues: </p>
                         <p>{alertSettings.criteriaData.length} issues</p>
                     </div>
                     <div>
-                        <button onClick={setCriteria}>Set Criteria</button>
+                        <button style={Button.blue} onClick={setCriteria}>Set Criteria</button>
                     </div>
                     
                 </article>
-                <section id='alertDetails'>
-                    <div>
+                
+                <section id='alertDetails' style={TagSectionsCSS.management.secRight}>
+                    <div style={TagSectionsCSS.management.titles}>
                         <h2>Issues Related to Criteria</h2>
                     </div>
                     {alertSettings.criteriaData && (
                         alertSettings.criteriaData.map((criteriaInfo) => {
                             return (
-                                <article key={criteriaInfo._id} id={"CRI" + criteriaInfo._id}>
-                                    <div>
-                                        <p><BsPersonCircle /> {criteriaInfo.user ? criteriaInfo.user.username : "Anonymouse"}</p>
+                                <article key={criteriaInfo._id} id={"CRI" + criteriaInfo._id} style={TagSectionsCSS.management.gapper}>
+                                    <div style={TagSectionsCSS.management.confirmations}>
+                                        <p style={TagSectionsCSS.management.bold}><BsPersonCircle /> {criteriaInfo.user ? criteriaInfo.user.username : "Anonymouse"}</p>
                                         <p>{GetDate(criteriaInfo.date)}</p>
                                     </div>
-                                    <div>
-                                        <h4>{criteriaInfo.description}; </h4>
+                                    <div style={TagSectionsCSS.management.gapperDesc}>
+                                        <p><AiFillMessage /> {criteriaInfo.description}; </p>
                                     </div>
-                                    <div>
-                                        <div>
-                                            <p title='This tag is legitimate'>Denied by users: </p>
-                                            <p>{criteriaInfo.confirmed}</p>
+                                    <div style={TagSectionsCSS.management.confirmationsManagement}>
+                                        <div style={TagSectionsCSS.management.confirmations}>
+                                            <p title='This tag is legitimate'style={TagSectionsCSS.management.bold}>Denied by users: </p>
+                                            <p>{criteriaInfo.confirmed} times</p>
                                         </div>
-                                        <div>
-                                            <p title='This is a fabrication'>Confirmed by users: </p>
-                                            <p>{criteriaInfo.denied}</p>
+                                        <div style={TagSectionsCSS.management.confirmations}>
+                                            <p title='This is a fabrication'style={TagSectionsCSS.management.bold}>Confirmed by users: </p>
+                                            <p>{criteriaInfo.denied} times</p>
                                         </div>
                                     </div>
                                 </article>
